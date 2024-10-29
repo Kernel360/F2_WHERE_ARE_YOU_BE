5bf1ce0139427c33c9e21938b2c67887aa8c1cd3
I-migi <sunnn10189@gmail.com>
2024-10-29 15:25:42 +0900

    feat: 리프레시 토큰 구현

--- api/src/main/java/org/badminton/api/filter/JwtAuthenticationFilter.java
+++ api/src/main/java/org/badminton/api/filter/JwtAuthenticationFilter.java
@@ -3,6 +3,7 @@ package org.badminton.api.filter;
 import java.io.IOException;
 import java.util.List;
 
+import org.badminton.api.config.security.SecurityUtil;
 import org.badminton.api.interfaces.auth.dto.CustomOAuth2Member;
 import org.badminton.api.interfaces.auth.jwt.JwtUtil;
 import org.badminton.api.interfaces.member.dto.MemberResponse;
@@ -11,9 +12,11 @@ import org.badminton.domain.domain.clubmember.entity.ClubMember;
 import org.badminton.domain.domain.member.entity.MemberAuthorization;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -31,41 +34,60 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
-		String token = jwtUtil.extractAccessTokenFromCookie(request);
 
-		if (token != null && jwtUtil.validateToken(token)) {
-			String memberToken = jwtUtil.getMemberToken(token);
-			List<ClubMember> clubMemberEntities = clubMemberReader.getClubMembersByMemberToken(memberToken);
-
-			String oAuthAccessToken = jwtUtil.getOAuthToken(token);
-
-			MemberResponse memberResponse = new MemberResponse(memberToken,
-				MemberAuthorization.AUTHORIZATION_USER.toString(), null, null, null, null);
-			CustomOAuth2Member customOAuth2Member = new CustomOAuth2Member(memberResponse,
-				jwtUtil.getRegistrationId(token), oAuthAccessToken);
-
-			for (ClubMember clubMember : clubMemberEntities) {
-				if (!clubMember.getClub().isClubDeleted()) {
-					customOAuth2Member.addClubRole(clubMember.getClub().getClubToken(), clubMember.getRole().name());
-					break;
-				}
-			}
-
-			Authentication authToken = new UsernamePasswordAuthenticationToken(customOAuth2Member, null,
-				customOAuth2Member.getAuthorities());
-
-			SecurityContextHolder.getContext().setAuthentication(authToken);
+		String accessToken = jwtUtil.extractAccessTokenFromCookie(request);
+		String oauthToken = jwtUtil.extractOauthTokenFromCookie(request);
+		if (accessToken == null || oauthToken == null) {
+			SecurityContextHolder.clearContext();
+			filterChain.doFilter(request, response);
 		}
+		SecurityContext context = SecurityContextHolder.getContext();
+		try {
+			if (jwtUtil.refreshAccessToken(context, accessToken, response) && jwtUtil.validate(oauthToken)) {
 
+				String memberToken = jwtUtil.getMemberToken(accessToken);
+				String registrationId = jwtUtil.getRegistrationId(oauthToken);
+				String oAuthAccessToken = jwtUtil.getOAuthToken(oauthToken);
+
+				processAuthenticationWithMemberToken(memberToken, registrationId, oAuthAccessToken);
+				filterChain.doFilter(request, response);
+			}
+		} catch (ExpiredJwtException expiredJwtException) {
+			String memberToken = expiredJwtException.getClaims().get("memberToken", String.class);
+			processAuthenticationWithMemberToken(memberToken, null, null);
+		}
 		filterChain.doFilter(request, response);
+	}
+
+	private void processAuthenticationWithMemberToken(String memberToken, String registrationId,
+		String oAuthAccessToken) {
+		List<ClubMember> clubMemberEntities = clubMemberReader.getClubMembersByMemberToken(memberToken);
+
+		MemberResponse memberResponse = new MemberResponse(memberToken,
+			MemberAuthorization.AUTHORIZATION_USER.toString(),
+			null, null, null, null);
+
+		CustomOAuth2Member customOAuth2Member = new CustomOAuth2Member(
+			memberResponse, registrationId, oAuthAccessToken);
+
+		clubMemberEntities.stream()
+			.filter(clubMember -> !clubMember.getClub().isClubDeleted())
+			.forEach(clubMember -> customOAuth2Member.addClubRole(
+				clubMember.getClub().getClubToken(),
+				clubMember.getRole().name()
+			));
+
+		Authentication authToken = new UsernamePasswordAuthenticationToken(
+			customOAuth2Member,
+			null,
+			customOAuth2Member.getAuthorities()
+		);
+
+		SecurityContextHolder.getContext().setAuthentication(authToken);
 	}
 
 	@Override
 	protected boolean shouldNotFilter(HttpServletRequest request) {
-		String path = request.getRequestURI();
-		return path.equals("/") || path.equals("/groups") || path.startsWith("/oauth2") || path.startsWith("/login")
-			|| path.startsWith("/api") || path.startsWith("/swagger-ui") || path.startsWith("/v3/api-docs")
-			;
+		return SecurityUtil.isPublicPath(request);
 	}
-
 }
--- api/src/main/java/org/badminton/api/interfaces/auth/jwt/JwtUtil.java
+++ api/src/main/java/org/badminton/api/interfaces/auth/jwt/JwtUtil.java
@@ -1,6 +1,7 @@
 package org.badminton.api.interfaces.auth.jwt;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -8,11 +9,14 @@ import java.util.List;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
+import org.badminton.api.interfaces.auth.dto.CustomOAuth2Member;
+import org.badminton.domain.domain.member.MemberReader;
+import org.badminton.domain.domain.member.entity.Member;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Component;
 
-import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,55 +26,48 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @Slf4j
 public class JwtUtil {
-	private static final long ACCESS_TOKEN_EXPIRY = 60 * 60 * 1000L; // 1시간
-	private static final long REFRESH_TOKEN_EXPIRY = 14 * 24 * 60 * 60 * 1000L; // 14일
+	private static final long ACCESS_TOKEN_EXPIRY = 30 * 60 * 1000L;
+	private static final long REFRESH_TOKEN_EXPIRY = 7 * 24 * 60 * 60 * 1000L;
+
 	private final SecretKey secretKey;
+	private final MemberReader memberReader;
 	@Value("${custom.server.domain}")
 	private String domain;
 
-	public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
+	public JwtUtil(@Value("${spring.jwt.secret}") String secret, MemberReader memberReader) {
 		secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
 			Jwts.SIG.HS256.key().build().getAlgorithm());
+		this.memberReader = memberReader;
 	}
 
-	public String createAccessToken(String memberToken, List<String> roles, String registrationId,
-		String oAuthAccessToken) {
+	public String createAccessToken(String memberToken, List<String> roles) {
 
 		return Jwts.builder()
 			.claim("memberToken", memberToken)
 			.claim("roles", String.join(",", roles))
-			.claim("registrationId", registrationId)
-			.claim("oAuthAccessToken", oAuthAccessToken)
 			.issuedAt(new Date(System.currentTimeMillis()))
 			.expiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRY))
 			.signWith(secretKey)
 			.compact();
 	}
 
-	public String createRefreshToken(String memberToken, List<String> roles, String registrationId) {
+	public String createOauthToken(String registrationId, String oAuthAccessToken) {
 		return Jwts.builder()
-			.claim("memberToken", memberToken)
-			.claim("roles", String.join(",", roles))
 			.claim("registrationId", registrationId)
+			.claim("oAuthAccessToken", oAuthAccessToken)
+			.issuedAt(new Date(System.currentTimeMillis()))
 			.expiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRY))
 			.signWith(secretKey)
 			.compact();
 	}
 
-	public void setAccessTokenHeader(HttpServletResponse response, String accessToken) {
-		response.setHeader("Authorization", "Bearer %s".formatted(accessToken));
-	}
-
-	public void setRefreshTokenCookie(HttpServletResponse response, String refreshToken) {
-		ResponseCookie cookie = ResponseCookie.from("refresh_token", refreshToken)
-			.httpOnly(true)
-			.secure(true)
-			.sameSite("None")
-			.domain(domain)
-			.path("/")
-			.maxAge(REFRESH_TOKEN_EXPIRY) // 14일
-			.build();
-		response.addHeader("Set-Cookie", cookie.toString());
+	public String createRefreshToken(String memberToken) {
+		return Jwts.builder()
+			.claim("memberToken", memberToken)
+			.issuedAt(new Date(System.currentTimeMillis()))
+			.expiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRY))
+			.signWith(secretKey)
+			.compact();
 	}
 
 	public void setAccessTokenCookie(HttpServletResponse response, String accessToken) {
@@ -85,13 +82,24 @@ public class JwtUtil {
 		response.addHeader("Set-Cookie", cookie.toString());
 	}
 
+	public void setOauthTokenCookie(HttpServletResponse response, String oauthToken) {
+		ResponseCookie cookie = ResponseCookie.from("oauth_token", oauthToken)
+			.httpOnly(true)
+			.secure(true)
+			.sameSite("None")
+			.domain(domain)
+			.path("/")
+			.maxAge(REFRESH_TOKEN_EXPIRY / 1000) // 14일
+			.build();
+		response.addHeader("Set-Cookie", cookie.toString());
+	}
+
 	public String getMemberToken(String token) {
 		return getDetail(token, "memberToken");
 	}
 
 	public String getRegistrationId(String token) {
 		return getDetail(token, "registrationId");
-
 	}
 
 	public String getOAuthToken(String token) {
@@ -112,48 +120,89 @@ public class JwtUtil {
 			.get(details, String.class);
 	}
 
-	public String extractAccessTokenFromHeader(HttpServletRequest request) {
-		String bearerToken = request.getHeader("Authorization");
-		if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-			return bearerToken.substring(7);
-		}
-		return null;
-	}
-
-	public String extractRefreshTokenFromCookie(HttpServletRequest request) {
-		Cookie[] cookies = request.getCookies();
-		if (cookies != null) {
-			for (Cookie cookie : cookies) {
-				if ("refresh_token".equals(cookie.getName())) {
-					return cookie.getValue();
-				}
-			}
-		}
-		return null;
-	}
-
 	public String extractAccessTokenFromCookie(HttpServletRequest request) {
 		Cookie[] cookies = request.getCookies();
-		if (cookies != null) {
-			for (Cookie cookie : cookies) {
-				if ("access_token".equals(cookie.getName())) {
-					return cookie.getValue();
-				}
-			}
-		}
-		return null;
+		return cookieValue(cookies, "access_token");
 	}
 
-	public boolean validateToken(String token) {
-		try {
-			Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token);
+	public String extractOauthTokenFromCookie(HttpServletRequest request) {
+		Cookie[] cookies = request.getCookies();
+		return cookieValue(cookies, "oauth_token");
+	}
+
+	public String cookieValue(Cookie[] cookies, String key) {
+		return Arrays.stream(cookies)
+			.filter(cookie -> key.equals(cookie.getName()))
+			.map(Cookie::getValue)
+			.findFirst()
+			.orElse(null);
+	}
+
+	public void removeAccessTokenCookie(HttpServletResponse response) {
+		ResponseCookie cookie = ResponseCookie.from("access_token", "")
+			.httpOnly(true)
+			.secure(true)
+			.sameSite("None")
+			.domain(domain)
+			.path("/")
+			.maxAge(0) // 쿠키 삭제
+			.build();
+		response.addHeader("Set-Cookie", cookie.toString());
+	}
+
+	public void removeOauthTokenCookie(HttpServletResponse response) {
+		ResponseCookie cookie = ResponseCookie.from("oauth_token", "")
+			.httpOnly(true)
+			.secure(true)
+			.sameSite("None")
+			.domain(domain)
+			.path("/")
+			.maxAge(0) // 쿠키 삭제
+			.build();
+		response.addHeader("Set-Cookie", cookie.toString());
+	}
+
+	public boolean refreshAccessToken(SecurityContext context, String accessToken, HttpServletResponse response) {
+		if (validate(accessToken)) {
 			return true;
-		} catch (ExpiredJwtException e) {
-			log.error("JWT token is expired: {}", e.getMessage());
-			return false;
-		} catch (Exception e) {
-			log.error("Invalid JWT token: {}", e.getMessage());
+		}
+		Object details = context.getAuthentication().getDetails();
+
+		String memberToken = ((CustomOAuth2Member)details).getMemberToken();
+		Member member = memberReader.getMember(memberToken);
+
+		String refreshToken = member.getRefreshToken();
+
+		if (validate(refreshToken)) {
+			List<String> roles = getRoles(refreshToken);
+			String newAccessToken = createAccessToken(memberToken, roles);
+			setAccessTokenCookie(response, newAccessToken);
+			return true;
+		}
+
+		removeAccessTokenCookie(response);
+		removeOauthTokenCookie(response);
+
+		return false;
+
+	}
+
+	public boolean validate(String token) {
+		var claims = Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload();
+
+		// 현재 시간과 만료 시간을 비교
+		Date expiration = claims.getExpiration();
+		Date now = new Date();
+
+		if (expiration == null || now.after(expiration)) {
 			return false;
 		}
+
+		return true;
+
 	}
 }
