b64da2c76987c6c43dc7c14c59d49e0904714db0
I-migi <sunnn10189@gmail.com>
2024-11-05 16:48:17 +0900

    fix: participationLimit 에러 구현

---
+++ domain/src/main/java/org/badminton/domain/common/exception/league/ParticipationLimitReachedException.java
@@ -0,0 +1,14 @@
+package org.badminton.domain.common.exception.league;
+
+import org.badminton.domain.common.error.ErrorCode;
+import org.badminton.domain.common.exception.BadmintonException;
+
+public class ParticipationLimitReachedException extends BadmintonException {
+	public ParticipationLimitReachedException(Long leagueId) {
+		super(ErrorCode.LEAGUE_RECRUITING_ALREADY_COMPLETED, "[경기 일정 아이디: " + leagueId + " ]");
+	}
+
+	public ParticipationLimitReachedException(Long leagueId, Exception e) {
+		super(ErrorCode.LEAGUE_RECRUITING_ALREADY_COMPLETED, "[경기 일정 아이디: " + leagueId + " ]", e);
+	}
+}
--- domain/src/main/java/org/badminton/domain/domain/league/LeagueParticipantServiceImpl.java
+++ domain/src/main/java/org/badminton/domain/domain/league/LeagueParticipantServiceImpl.java
@@ -1,12 +1,11 @@
 package org.badminton.domain.domain.league;
 
-import jakarta.transaction.Transactional;
 import java.util.Objects;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.badminton.domain.common.exception.league.LeagueCannotBeCanceledWhenIsNotRecruiting;
 import org.badminton.domain.common.exception.league.LeagueOwnerCannotCancelLeagueParticipationException;
 import org.badminton.domain.common.exception.league.LeagueParticipationDuplicateException;
+import org.badminton.domain.common.exception.league.ParticipationLimitReachedException;
 import org.badminton.domain.domain.clubmember.ClubMemberReader;
 import org.badminton.domain.domain.clubmember.entity.ClubMember;
 import org.badminton.domain.domain.league.entity.League;
@@ -16,50 +15,58 @@ import org.badminton.domain.domain.league.info.LeagueParticipantCancelInfo;
 import org.badminton.domain.domain.league.info.LeagueParticipantInfo;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class LeagueParticipantServiceImpl implements LeagueParticipantService {
-    private final LeagueParticipantReader leagueParticipantReader;
-    private final LeagueParticipantStore leagueParticipantStore;
-    private final LeagueReader leagueReader;
-    private final ClubMemberReader clubMemberReader;
+	private final LeagueParticipantReader leagueParticipantReader;
+	private final LeagueParticipantStore leagueParticipantStore;
+	private final LeagueReader leagueReader;
+	private final ClubMemberReader clubMemberReader;
 
-    @Override
-    @Transactional
-    public boolean isParticipant(String memberToken, Long leagueId) {
-        return leagueParticipantReader.isParticipant(memberToken, leagueId);
-    }
+	@Override
+	@Transactional
+	public boolean isParticipant(String memberToken, Long leagueId) {
+		return leagueParticipantReader.isParticipant(memberToken, leagueId);
+	}
 
-    @Override
-    @Transactional
-    public int countParticipantMember(Long leagueId) {
-        return leagueParticipantReader.countParticipantMember(leagueId);
-    }
+	@Override
+	@Transactional
+	public int countParticipantMember(Long leagueId) {
+		return leagueParticipantReader.countParticipantMember(leagueId);
+	}
 
-    @Override
-    public LeagueParticipantCancelInfo participantLeagueCancel(String memberToken, String clubToken, Long leagueId) {
-        League league = leagueReader.readLeagueById(leagueId);
-        if (Objects.equals(league.getLeagueOwnerMemberToken(), memberToken)) {
-            throw new LeagueOwnerCannotCancelLeagueParticipationException(memberToken, leagueId);
-        }
-        if (league.getLeagueStatus() != LeagueStatus.RECRUITING) {
-            throw new LeagueCannotBeCanceledWhenIsNotRecruiting(leagueId, league.getLeagueStatus());
-        }
-        ClubMember clubMember = clubMemberReader.getClubMemberByMemberTokenAndClubToken(clubToken, memberToken);
-        Long clubMemberId = clubMember.getClubMemberId();
-        LeagueParticipant leagueParticipant = leagueParticipantReader.findParticipant(leagueId, clubMemberId);
-        var result = leagueParticipantStore.cancelStore(leagueParticipant);
-        return LeagueParticipantCancelInfo.from(result);
-    }
+	@Override
+	public LeagueParticipantCancelInfo participantLeagueCancel(String memberToken, String clubToken, Long leagueId) {
+		League league = leagueReader.readLeagueById(leagueId);
+		if (Objects.equals(league.getLeagueOwnerMemberToken(), memberToken)) {
+			throw new LeagueOwnerCannotCancelLeagueParticipationException(memberToken, leagueId);
+		}
+		if (league.getLeagueStatus() != LeagueStatus.RECRUITING) {
+			throw new LeagueCannotBeCanceledWhenIsNotRecruiting(leagueId, league.getLeagueStatus());
+		}
+		ClubMember clubMember = clubMemberReader.getClubMemberByMemberTokenAndClubToken(clubToken, memberToken);
+		Long clubMemberId = clubMember.getClubMemberId();
+		LeagueParticipant leagueParticipant = leagueParticipantReader.findParticipant(leagueId, clubMemberId);
+		var result = leagueParticipantStore.cancelStore(leagueParticipant);
+		return LeagueParticipantCancelInfo.from(result);
+	}
 
-    @Override
-    public LeagueParticipantInfo participantInLeague(String memberToken, String clubToken, Long leagueId) {
-        ClubMember clubMember = clubMemberReader.getClubMemberByMemberTokenAndClubToken(clubToken, memberToken);
-        League league = leagueReader.readLeagueById(leagueId);
-        if (leagueParticipantReader.isParticipant(memberToken, leagueId)) {
-            throw new LeagueParticipationDuplicateException(leagueId, memberToken);
-        }
-        return LeagueParticipantInfo.from(leagueParticipantStore.store(clubMember, league));
-    }
+	@Override
+	public LeagueParticipantInfo participantInLeague(String memberToken, String clubToken, Long leagueId) {
+		ClubMember clubMember = clubMemberReader.getClubMemberByMemberTokenAndClubToken(clubToken, memberToken);
+		League league = leagueReader.readLeagueById(leagueId);
+		if (leagueParticipantReader.isParticipant(memberToken, leagueId)) {
+			throw new LeagueParticipationDuplicateException(leagueId, memberToken);
+		}
+		if (!(league.getPlayerLimitCount() > leagueParticipantReader.countParticipantMember(league.getLeagueId()))) {
+			throw new ParticipationLimitReachedException(league.getLeagueId());
+		}
+
+		return LeagueParticipantInfo.from(leagueParticipantStore.store(clubMember, league));
+	}
 }
