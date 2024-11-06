e71cd17659bcc9cbd776295e7678c19f714f65d7
Lee kangmin <km2535@naver.com>
2024-11-06 11:48:04 +0900

    refactor: 동호회 별 클럽맴버 리턴 값 수정

--- api/src/main/java/org/badminton/api/interfaces/club/dto/ClubCardResponse.java
+++ api/src/main/java/org/badminton/api/interfaces/club/dto/ClubCardResponse.java
@@ -1,11 +1,6 @@
 package org.badminton.api.interfaces.club.dto;
 
 import java.time.LocalDateTime;
-import java.util.Map;
-
-import org.badminton.domain.domain.club.entity.Club;
-import org.badminton.domain.domain.club.ClubMemberCountByTier;
-import org.badminton.domain.domain.member.entity.Member;
 
 public record ClubCardResponse(
 	String clubToken,
@@ -14,16 +9,9 @@ public record ClubCardResponse(
 	String clubImage,
 	LocalDateTime createdAt,
 	LocalDateTime modifiedAt,
-	ClubMemberCountByTier clubMemberCountByTier
+	Long goldClubMemberCount,
+	Long silverClubMemberCount,
+	Long bronzeClubMemberCount
 ) {
 
-	public static ClubCardResponse clubEntityToClubsCardResponse(Club club,
-		Map<Member.MemberTier, Long> tierCounts) {
-		return new ClubCardResponse(club.getClubToken(), club.getClubName(), club.getClubDescription(),
-			club.getClubImage(),
-			club.getCreatedAt(),
-			club.getModifiedAt(),
-			ClubMemberCountByTier.ofClubMemberCountResponse(tierCounts)
-		);
-	}
 }
