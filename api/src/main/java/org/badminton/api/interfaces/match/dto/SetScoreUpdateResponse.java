0bde752d5c402ec8cf5061f8cb483bf7e1866c4c
soeunnPark <carol0724@naver.com>
2024-11-07 11:22:08 +0900

    docs: setscore update response에 스키마 추가

--- api/src/main/java/org/badminton/api/interfaces/match/dto/SetScoreUpdateResponse.java
+++ api/src/main/java/org/badminton/api/interfaces/match/dto/SetScoreUpdateResponse.java
@@ -1,13 +1,20 @@
 package org.badminton.api.interfaces.match.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import org.badminton.domain.common.enums.MatchType;
 import org.badminton.domain.domain.match.info.SetInfo;
 
 public record SetScoreUpdateResponse(
+        @Schema(description = "매치 아이디", requiredMode = RequiredMode.REQUIRED)
         Long matchId,
-        int setIndex,
+        @Schema(description = "세트 번호 (1 | 2 | 3)", requiredMode = RequiredMode.REQUIRED)
+        int setNumber,
+        @Schema(description = "점수 1", requiredMode = RequiredMode.REQUIRED)
         int score1,
+        @Schema(description = "점수 2", requiredMode = RequiredMode.REQUIRED)
         int score2,
+        @Schema(description = "매치 타입 (SINGLES | DOUBLES)", requiredMode = RequiredMode.REQUIRED)
         MatchType matchType
 ) {
 
