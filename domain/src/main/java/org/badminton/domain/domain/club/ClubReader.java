d6558aa0c03f1c6851218bfb12f879be55b7f45b
soeunnPark <carol0724@naver.com>
2024-11-13 17:22:59 +0900

    refactor: add custom exception for deleting league

--- domain/src/main/java/org/badminton/domain/domain/club/ClubReader.java
+++ domain/src/main/java/org/badminton/domain/domain/club/ClubReader.java
@@ -1,23 +1,24 @@
 package org.badminton.domain.domain.club;
 
 import java.util.List;
-
 import org.badminton.domain.domain.club.entity.Club;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ClubReader {
-	Page<Club> readAllClubs(Pageable pageable);
+    Page<Club> readAllClubs(Pageable pageable);
 
-	Page<Club> keywordSearch(String keyword, Pageable pageable);
+    Page<Club> keywordSearch(String keyword, Pageable pageable);
 
-	List<Club> readRecentlyClubs();
+    List<Club> readRecentlyClubs();
 
-	List<Club> readAllClubs();
+    List<Club> readAllClubs();
 
-	Club readClub(String clubToken);
+    Club readClub(String clubToken);
 
-	Club readClubByClubId(Long clubId);
+    Club readClubByClubId(Long clubId);
 
-	boolean UniqueClubName(String clubName);
+    Club readExistingClub(String clubToken);
+
+    boolean UniqueClubName(String clubName);
 }
--- infrastructure/src/main/java/org/badminton/infrastructure/club/ClubRepository.java
+++ infrastructure/src/main/java/org/badminton/infrastructure/club/ClubRepository.java
@@ -2,27 +2,24 @@ package org.badminton.infrastructure.club;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.badminton.domain.domain.club.entity.Club;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClubRepository extends JpaRepository<Club, Long> {
-	Optional<Club> findByClubToken(String clubToken);
+    Optional<Club> findByClubToken(String clubToken);
 
-	Optional<Club> findByClubNameAndIsClubDeletedFalse(String clubName);
+    Optional<Club> findByClubTokenAndIsClubDeletedFalse(String clubToken);
 
-	Optional<Club> findByClubIdAndIsClubDeletedFalse(Long clubId);
+    Page<Club> findAllByIsClubDeletedIsFalse(Pageable pageable);
 
-	Page<Club> findAllByIsClubDeletedIsFalse(Pageable pageable);
+    Page<Club> findAllByClubNameContainingIgnoreCaseAndIsClubDeletedIsFalse(String keyword, Pageable pageable);
 
-	Page<Club> findAllByClubNameContainingIgnoreCaseAndIsClubDeletedIsFalse(String keyword, Pageable pageable);
+    List<Club> findTop10ByIsClubDeletedIsFalseOrderByCreatedAtDesc();
 
-	List<Club> findTop10ByIsClubDeletedIsFalseOrderByCreatedAtDesc();
+    Club findByClubId(Long clubId);
 
-	Club findByClubId(Long clubId);
-
-	boolean existsByClubNameAndIsClubDeletedFalse(String clubName);
+    boolean existsByClubNameAndIsClubDeletedFalse(String clubName);
 }
 
