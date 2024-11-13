159a8d700ee276311f55a3e1b863702de947f2ca
I-migi <sunnn10189@gmail.com>
2024-11-13 10:42:16 +0900

    refactor: 동호회 회원 전체 조회시 경기전적과 티어 response에 추가

--- api/src/main/java/org/badminton/api/interfaces/clubmember/dto/ClubMemberResponse.java
+++ api/src/main/java/org/badminton/api/interfaces/clubmember/dto/ClubMemberResponse.java
@@ -1,27 +1,36 @@
 package org.badminton.api.interfaces.clubmember.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+import org.badminton.api.interfaces.league.dto.LeagueRecordResponse;
 import org.badminton.domain.domain.clubmember.entity.ClubMember;
 import org.badminton.domain.domain.clubmember.info.ClubMemberInfo;
+import org.badminton.domain.domain.member.entity.Member;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 
 public record ClubMemberResponse(
-        @Schema(requiredMode = RequiredMode.REQUIRED)
-        Long clubMemberId,
-        @Schema(requiredMode = RequiredMode.REQUIRED)
-        String image,
-        @Schema(requiredMode = RequiredMode.REQUIRED)
-        String name,
-        @Schema(requiredMode = RequiredMode.REQUIRED)
-        ClubMember.ClubMemberRole role
+	@Schema(description = "동호회 회원 ID", requiredMode = RequiredMode.REQUIRED)
+	Long clubMemberId,
+	@Schema(description = "동호회 회원 이미지", requiredMode = RequiredMode.REQUIRED)
+	String image,
+	@Schema(description = "동호회 회원 이름", requiredMode = RequiredMode.REQUIRED)
+	String name,
+	@Schema(description = "동호회 회원 role", requiredMode = RequiredMode.REQUIRED)
+	ClubMember.ClubMemberRole role,
+	@Schema(description = "동호회 회원 경기 전적", requiredMode = RequiredMode.REQUIRED)
+	LeagueRecordResponse leagueRecord,
+	@Schema(description = "동호회 회원 티어", requiredMode = RequiredMode.REQUIRED)
+	Member.MemberTier tier
 ) {
 
-    public static ClubMemberResponse entityToClubMemberResponse(ClubMemberInfo clubMemberInfo) {
-        return new ClubMemberResponse(
-                clubMemberInfo.clubMemberId(),
-                clubMemberInfo.image(),
-                clubMemberInfo.name(),
-                clubMemberInfo.role()
-        );
-    }
+	public static ClubMemberResponse entityToClubMemberResponse(ClubMemberInfo clubMemberInfo) {
+		return new ClubMemberResponse(
+			clubMemberInfo.clubMemberId(),
+			clubMemberInfo.image(),
+			clubMemberInfo.name(),
+			clubMemberInfo.role(),
+			LeagueRecordResponse.toLeagueRecordResponse(clubMemberInfo.leagueRecord()),
+			clubMemberInfo.tier()
+		);
+	}
 }
--- domain/src/main/java/org/badminton/domain/domain/clubmember/info/ClubMemberInfo.java
+++ domain/src/main/java/org/badminton/domain/domain/clubmember/info/ClubMemberInfo.java
@@ -1,19 +1,25 @@
 package org.badminton.domain.domain.clubmember.info;
 
 import org.badminton.domain.domain.clubmember.entity.ClubMember;
+import org.badminton.domain.domain.league.info.LeagueRecordInfo;
+import org.badminton.domain.domain.member.entity.Member;
 
 public record ClubMemberInfo(
 	Long clubMemberId,
 	String image,
 	String name,
-	ClubMember.ClubMemberRole role
+	ClubMember.ClubMemberRole role,
+	LeagueRecordInfo leagueRecord,
+	Member.MemberTier tier
 ) {
 	public static ClubMemberInfo valueOf(ClubMember clubMember) {
 		return new ClubMemberInfo(
 			clubMember.getClubMemberId(),
 			clubMember.getMember().getProfileImage(),
 			clubMember.getMember().getName(),
-			clubMember.getRole()
+			clubMember.getRole(),
+			LeagueRecordInfo.from(clubMember.getMember().getLeagueRecord()),
+			clubMember.getMember().getTier()
 		);
 	}
 }
