f742fba2c5e020794d219e4997452d4dd4c7e661
soeunnPark <carol0724@naver.com>
2024-11-14 11:00:49 +0900

    refactor: league create request 스키마 수정

--- domain/src/main/java/org/badminton/domain/domain/club/entity/Club.java
+++ domain/src/main/java/org/badminton/domain/domain/club/entity/Club.java
@@ -1,17 +1,6 @@
 package org.badminton.domain.domain.club.entity;
 
-import static org.badminton.domain.domain.club.info.ClubSummaryInfo.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import org.badminton.domain.common.AbstractBaseTime;
-import org.badminton.domain.common.TokenGenerator;
-import org.badminton.domain.domain.club.command.ClubUpdateCommand;
-import org.badminton.domain.domain.club.info.ClubCreateInfo;
-import org.badminton.domain.domain.clubmember.entity.ClubMember;
-import org.badminton.domain.domain.member.entity.Member;
+import static org.badminton.domain.domain.club.info.ClubSummaryInfo.getMemberTierLongMap;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -21,10 +10,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.badminton.domain.common.AbstractBaseTime;
+import org.badminton.domain.common.TokenGenerator;
+import org.badminton.domain.domain.club.command.ClubUpdateCommand;
+import org.badminton.domain.domain.club.info.ClubCreateInfo;
+import org.badminton.domain.domain.clubmember.entity.ClubMember;
+import org.badminton.domain.domain.member.entity.Member;
 
 @Entity
 @Getter
@@ -32,67 +30,67 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Club extends AbstractBaseTime {
-	public static final String CLUB_PREFIX = "club_";
+    public static final String CLUB_PREFIX = "club_";
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long clubId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clubId;
 
-	@Column(name = "clubToken")
-	private String clubToken;
+    @Column(name = "clubToken")
+    private String clubToken;
 
-	private String clubName;
+    private String clubName;
 
-	@Column(columnDefinition = "text")
-	private String clubDescription;
+    @Column(columnDefinition = "TEXT", length = 1000)
+    private String clubDescription;
 
-	private String clubImage;
+    private String clubImage;
 
-	private boolean isClubDeleted;
+    private boolean isClubDeleted;
 
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "club")
-	private List<ClubMember> clubMembers = new ArrayList<>();
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "club")
+    private List<ClubMember> clubMembers = new ArrayList<>();
 
-	public Club(String clubName, String clubDescription, String clubImage) {
-		super();
-		this.clubToken = TokenGenerator.randomCharacterWithPrefix(CLUB_PREFIX);
-		this.clubName = clubName;
-		this.clubDescription = clubDescription;
-		this.clubImage = clubImage;
-		isClubDeleted = false;
-	}
+    public Club(String clubName, String clubDescription, String clubImage) {
+        super();
+        this.clubToken = TokenGenerator.randomCharacterWithPrefix(CLUB_PREFIX);
+        this.clubName = clubName;
+        this.clubDescription = clubDescription;
+        this.clubImage = clubImage;
+        isClubDeleted = false;
+    }
 
-	public Club(ClubCreateInfo clubCreateInfo) {
-		this.clubId = clubCreateInfo.clubId();
-		this.clubDescription = clubCreateInfo.clubDescription();
-		this.clubName = clubCreateInfo.clubName();
-		this.clubImage = clubCreateInfo.clubImage();
-		this.clubToken = clubCreateInfo.clubToken();
-		this.isClubDeleted = false;
-	}
+    public Club(ClubCreateInfo clubCreateInfo) {
+        this.clubId = clubCreateInfo.clubId();
+        this.clubDescription = clubCreateInfo.clubDescription();
+        this.clubName = clubCreateInfo.clubName();
+        this.clubImage = clubCreateInfo.clubImage();
+        this.clubToken = clubCreateInfo.clubToken();
+        this.isClubDeleted = false;
+    }
 
-	public Club(Long clubId, String clubToken, String clubName, String clubDescription, String clubImage,
-		List<ClubMember> clubMembers) {
-		this.clubId = clubId;
-		this.clubToken = clubToken;
-		this.clubName = clubName;
-		this.clubDescription = clubDescription;
-		this.clubImage = clubImage;
-		this.clubMembers = clubMembers;
-		isClubDeleted = false;
-	}
+    public Club(Long clubId, String clubToken, String clubName, String clubDescription, String clubImage,
+                List<ClubMember> clubMembers) {
+        this.clubId = clubId;
+        this.clubToken = clubToken;
+        this.clubName = clubName;
+        this.clubDescription = clubDescription;
+        this.clubImage = clubImage;
+        this.clubMembers = clubMembers;
+        isClubDeleted = false;
+    }
 
-	public void updateClub(ClubUpdateCommand clubUpdateCommand) {
-		this.clubName = clubUpdateCommand.clubName();
-		this.clubDescription = clubUpdateCommand.clubDescription();
-		this.clubImage = clubUpdateCommand.clubImage();
-	}
+    public void updateClub(ClubUpdateCommand clubUpdateCommand) {
+        this.clubName = clubUpdateCommand.clubName();
+        this.clubDescription = clubUpdateCommand.clubDescription();
+        this.clubImage = clubUpdateCommand.clubImage();
+    }
 
-	public Map<Member.MemberTier, Long> getClubMemberCountByTier() {
-		return getMemberTierLongMap(clubMembers);
-	}
+    public Map<Member.MemberTier, Long> getClubMemberCountByTier() {
+        return getMemberTierLongMap(clubMembers);
+    }
 
-	public void doWithdrawal() {
-		this.isClubDeleted = true;
-	}
+    public void doWithdrawal() {
+        this.isClubDeleted = true;
+    }
 }
