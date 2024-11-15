955e76ffde95d36864ea63ce4f82fcf987ec0d92
Lee kangmin <km2535@naver.com>
2024-11-15 14:59:49 +0900

    feat : 리그 중복 생성 방지

--- domain/src/main/java/org/badminton/domain/common/exception/league/LeagueCreationWithin3HoursException.java
+++ domain/src/main/java/org/badminton/domain/common/exception/league/LeagueCreationWithin3HoursException.java
@@ -7,6 +7,6 @@ import org.badminton.domain.common.exception.BadmintonException;
 
 public class LeagueCreationWithin3HoursException extends BadmintonException {
 	public LeagueCreationWithin3HoursException(LocalDateTime localDateTime) {
-		super(ErrorCode.LEAGUE_NOT_ALLOW_CREATE, "[경기 일정 : " + localDateTime + "]");
+		super(ErrorCode.LEAGUE_AT_LESS_THAN_THREE_HOUR_INTERVAL, "[경기 일정 : " + localDateTime + "]");
 	}
 }
