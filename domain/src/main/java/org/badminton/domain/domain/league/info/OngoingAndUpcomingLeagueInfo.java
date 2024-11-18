01c776a76a21dd92d64fd182cf31aaf57f7b80a6
I-migi <sunnn10189@gmail.com>
2024-11-18 18:39:46 +0900

    fix: response

--- domain/src/main/java/org/badminton/domain/domain/league/info/OngoingAndUpcomingLeagueInfo.java
+++ domain/src/main/java/org/badminton/domain/domain/league/info/OngoingAndUpcomingLeagueInfo.java
@@ -1,35 +1,40 @@
 package org.badminton.domain.domain.league.info;
 
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+
 import org.badminton.domain.common.enums.MatchType;
 import org.badminton.domain.domain.league.entity.League;
 import org.badminton.domain.domain.league.enums.LeagueStatus;
+import org.badminton.domain.domain.member.entity.Member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class OngoingAndUpcomingLeagueInfo {
 
-    Long leagueId;
-    LocalDateTime leagueAt;
-    String leagueName;
-    String description;
-    MatchType matchType;
-    int playerLimitCount;
-    int recruitedMemberCount;
-    LeagueStatus leagueStatus;
+	Long leagueId;
+	LocalDateTime leagueAt;
+	String leagueName;
+	String description;
+	MatchType matchType;
+	int playerLimitCount;
+	int recruitedMemberCount;
+	LeagueStatus leagueStatus;
+	Member.MemberTier requiredTier;
 
-    public static OngoingAndUpcomingLeagueInfo from(League league, int recruitedMemberCount) {
-        return new OngoingAndUpcomingLeagueInfo(
-                league.getLeagueId(),
-                league.getLeagueAt(),
-                league.getLeagueName(),
-                league.getDescription(),
-                league.getMatchType(),
-                league.getPlayerLimitCount(),
-                recruitedMemberCount,
-                league.getLeagueStatus()
-        );
-    }
+	public static OngoingAndUpcomingLeagueInfo from(League league, int recruitedMemberCount) {
+		return new OngoingAndUpcomingLeagueInfo(
+			league.getLeagueId(),
+			league.getLeagueAt(),
+			league.getLeagueName(),
+			league.getDescription(),
+			league.getMatchType(),
+			league.getPlayerLimitCount(),
+			recruitedMemberCount,
+			league.getLeagueStatus(),
+			league.getRequiredTier()
+		);
+	}
 }
