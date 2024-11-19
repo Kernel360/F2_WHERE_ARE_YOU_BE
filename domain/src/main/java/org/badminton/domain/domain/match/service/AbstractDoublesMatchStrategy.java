3fd063239026b8f61fcbe325a7d9cbaf06baf2f7
soeunnPark <carol0724@naver.com>
2024-11-19 14:55:30 +0900

    refactor: 메인 페이지에 진행 중인 세트 점수 조회 시 매치 아이디 기준으로 정렬

--- domain/src/main/java/org/badminton/domain/domain/match/service/AbstractDoublesMatchStrategy.java
+++ domain/src/main/java/org/badminton/domain/domain/match/service/AbstractDoublesMatchStrategy.java
@@ -1,12 +1,12 @@
 package org.badminton.domain.domain.match.service;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import lombok.RequiredArgsConstructor;
 import org.badminton.domain.common.enums.MatchStatus;
-import org.badminton.domain.common.enums.SetStatus;
+import org.badminton.domain.common.error.ErrorCode;
+import org.badminton.domain.common.exception.BadmintonException;
 import org.badminton.domain.domain.league.entity.League;
 import org.badminton.domain.domain.league.entity.LeagueParticipant;
 import org.badminton.domain.domain.match.command.MatchCommand;
@@ -20,76 +20,83 @@ import org.badminton.domain.domain.match.info.SetInfo;
 import org.badminton.domain.domain.match.store.DoublesMatchReader;
 import org.springframework.stereotype.Service;
 
-import lombok.RequiredArgsConstructor;
-
 @Service
 @RequiredArgsConstructor
 public abstract class AbstractDoublesMatchStrategy implements MatchStrategy {
 
-	public static final int FIRST_ROUND_NUMBER = 1;
-	private final DoublesMatchReader doublesMatchReader;
+    public static final int FIRST_ROUND_NUMBER = 1;
+    private final DoublesMatchReader doublesMatchReader;
 
-	@Override
-	public abstract void checkDuplicateInitialBracket(Long leagueId);
+    @Override
+    public abstract void checkDuplicateInitialBracket(Long leagueId);
 
-	@Override
-	public BracketInfo retrieveBracketInLeague(Long leagueId) {
-		List<DoublesMatch> bracketInLeague = doublesMatchReader.getDoublesBracket(leagueId);
-		return BracketInfo.fromDoubles(FIRST_ROUND_NUMBER, bracketInLeague);
-	}
+    @Override
+    public BracketInfo retrieveBracketInLeague(Long leagueId) {
+        List<DoublesMatch> bracketInLeague = doublesMatchReader.getDoublesBracket(leagueId);
+        return BracketInfo.fromDoubles(FIRST_ROUND_NUMBER, bracketInLeague);
+    }
 
-	@Override
-	public List<SetInfo.Main> retrieveAllSetsScoreInLeague(Long leagueId) {
-		return doublesMatchReader.getDoublesBracket(leagueId).stream()
-			.flatMap(doublesMatch ->
-				doublesMatch.getDoublesSets().stream()
-					.map(doublesSet -> SetInfo.fromDoublesSet(doublesMatch.getId(),
-						doublesSet.getSetNumber(), doublesSet))
-			)
-			.toList();
-	}
+    @Override
+    public List<SetInfo.Main> retrieveAllSetsScoreInLeague(Long leagueId) {
+        return doublesMatchReader.getDoublesBracket(leagueId).stream()
+                .flatMap(doublesMatch ->
+                        doublesMatch.getDoublesSets().stream()
+                                .map(doublesSet -> SetInfo.fromDoublesSet(doublesMatch.getId(),
+                                        doublesSet.getSetNumber(), doublesSet))
+                )
+                .toList();
+    }
 
-	@Override
-	public MatchInfo.SetScoreDetails retrieveAllSetsScoreInMatch(Long matchId) {
-		DoublesMatch doublesMatch = doublesMatchReader.getDoublesMatch(matchId);
-		return MatchInfo.SetScoreDetails.fromDoublesMatchToMatchDetails(doublesMatch);
-	}
+    @Override
+    public MatchInfo.SetScoreDetails retrieveAllSetsScoreInMatch(Long matchId) {
+        DoublesMatch doublesMatch = doublesMatchReader.getDoublesMatch(matchId);
+        return MatchInfo.SetScoreDetails.fromDoublesMatchToMatchDetails(doublesMatch);
+    }
 
-	@Override
-	public abstract BracketInfo makeBracket(League league, List<LeagueParticipant> leagueParticipantList);
+    @Override
+    public abstract BracketInfo makeBracket(League league, List<LeagueParticipant> leagueParticipantList);
 
-	@Override
-	public SetInfo.Main registerSetScoreInMatch(Long matchId, Integer setIndex,
-		MatchCommand.UpdateSetScore updateSetScoreCommand) {
-		return null;
-	}
+    @Override
+    public SetInfo.Main registerSetScoreInMatch(Long matchId, Integer setIndex,
+                                                MatchCommand.UpdateSetScore updateSetScoreCommand) {
+        return null;
+    }
 
-	@Override
-	public boolean isMatchInLeague(Long leagueId) {
-		return !doublesMatchReader.checkIfBracketEmpty(leagueId);
-	}
+    @Override
+    public boolean isMatchInLeague(Long leagueId) {
+        return !doublesMatchReader.checkIfBracketEmpty(leagueId);
+    }
 
-	@Override
-	public List<LeagueSetsScoreInProgressInfo> retrieveLeagueSetsScoreInProgress(Long leagueId) {
-		List<DoublesMatch> doublesBracket = doublesMatchReader.getDoublesBracket(leagueId);
-		Map<DoublesMatch, DoublesSet> matchSetsInProgress = doublesBracket.stream()
-			.filter(doublesMatch -> doublesMatch.getMatchStatus() == MatchStatus.IN_PROGRESS)
-			.flatMap(doublesMatch -> doublesMatch.getDoublesSets().stream()
-				.filter(set -> set.getSetStatus() == SetStatus.IN_PROGRESS)
-				.findFirst()
-				.map(set -> Map.entry(doublesMatch, set))
-				.stream())
-			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    @Override
+    public List<LeagueSetsScoreInProgressInfo> retrieveLeagueSetsScoreInProgress(Long leagueId) {
+        List<DoublesMatch> doublesMatchInProgress = doublesMatchReader.getDoublesBracket(leagueId).stream()
+                .filter(doublesMatch -> doublesMatch.getMatchStatus() == MatchStatus.IN_PROGRESS)
+                .sorted(Comparator.comparing(DoublesMatch::getId))
+                .toList();
 
-		return matchSetsInProgress.entrySet().stream()
-			.map(entry -> LeagueSetsScoreInProgressInfo.fromDoublesMatchAndSet(entry.getKey(), entry.getValue()))
-			.toList();
-	}
+        List<DoublesSet> doublesSetInProgress = doublesMatchInProgress.stream()
+                .filter(doublesMatch -> doublesMatch.getSetInProgress().isPresent())
+                .map(doublesMatch -> doublesMatch.getSetInProgress().get())
+                .toList();
 
-	@Override
-	public MatchSetInfo retrieveMatchSet(Long matchId, int setNumber) {
-		DoublesMatch doublesMatch = doublesMatchReader.getDoublesMatch(matchId);
-		DoublesSet doublesSet = doublesMatch.getDoublesSet(setNumber);
-		return MatchSetInfo.fromDoubles(doublesSet);
-	}
+        if (doublesSetInProgress.size() != doublesMatchInProgress.size()) {
+            throw new BadmintonException(ErrorCode.INVALID_PARAMETER);
+        }
+
+        List<LeagueSetsScoreInProgressInfo> leagueSetsScoreInProgressInfo = new ArrayList<>();
+
+        for (int i = 0; i < doublesSetInProgress.size(); i++) {
+            leagueSetsScoreInProgressInfo.add(LeagueSetsScoreInProgressInfo.fromDoublesMatchAndSet(
+                    doublesMatchInProgress.get(i), doublesSetInProgress.get(i)
+            ));
+        }
+        return leagueSetsScoreInProgressInfo;
+    }
+
+    @Override
+    public MatchSetInfo retrieveMatchSet(Long matchId, int setNumber) {
+        DoublesMatch doublesMatch = doublesMatchReader.getDoublesMatch(matchId);
+        DoublesSet doublesSet = doublesMatch.getDoublesSet(setNumber);
+        return MatchSetInfo.fromDoubles(doublesSet);
+    }
 }
