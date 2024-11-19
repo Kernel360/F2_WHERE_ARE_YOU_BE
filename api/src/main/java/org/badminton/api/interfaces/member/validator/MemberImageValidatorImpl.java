61c7a55648198c15d60cb2a224b5c23919203fd1
I-migi <sunnn10189@gmail.com>
2024-11-19 17:41:10 +0900

    refactor: 이미지 검증 변경

--- api/src/main/java/org/badminton/api/interfaces/member/validator/MemberImageValidatorImpl.java
+++ api/src/main/java/org/badminton/api/interfaces/member/validator/MemberImageValidatorImpl.java
@@ -10,6 +10,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MemberImageValidatorImpl implements ConstraintValidator<MemberImageValidator, String> {
 
+	public static final String NAVER_DOMAIN = "pstatic.net";
+	public static final String GOOGLE_DOMAIN = "googleusercontent.com";
+	public static final String KAKAO_DOMAIN = "kakaocdn.net";
 	private static final String S3_DOMAIN = "d36om9pjoifd2y.cloudfront.net";
 	private static final Pattern IMAGE_PATTERN = Pattern.compile(
 		".*\\.(png|jpg|jpeg|gif|avif|svg|webp|bmp|ico|tiff|tif|heic|heif|raw|cr2|nef|arw|dng|psd|ai|eps|pdf|jfif|jpe|svgz|xbm|pgm|pbm|ppm|pnm|webm|apng)$",
@@ -27,9 +30,19 @@ public class MemberImageValidatorImpl implements ConstraintValidator<MemberImage
 		}
 		try {
 			URL url = new URL(value);
-			return url.getHost().equals(S3_DOMAIN) &&
-				url.getPath().startsWith("/member-profile/") &&
-				IMAGE_PATTERN.matcher(url.getPath()).matches();
+
+			// 허용된 호스트 목록
+			String host = url.getHost();
+			boolean isAllowedHostWithoutExtensionCheck =
+				host.endsWith(NAVER_DOMAIN) ||  // 네이버 도메인
+					host.endsWith(GOOGLE_DOMAIN) ||  // 구글 도메인
+					host.endsWith(KAKAO_DOMAIN);  // 카카오 도메인 (모든 하위 도메인 포함)
+
+			boolean isS3Valid = host.equals(S3_DOMAIN)
+				&& url.getPath().startsWith("/member-profile/")
+				&& IMAGE_PATTERN.matcher(url.getPath()).matches();
+
+			return isS3Valid || isAllowedHostWithoutExtensionCheck;
 		} catch (Exception exception) {
 			log.error(String.valueOf(exception));
 			return false;
