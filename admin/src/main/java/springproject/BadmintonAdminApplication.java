5b02745b75f63de605ee970eaf4503d384212ea4
I-migi <sunnn10189@gmail.com>
2024-11-21 17:14:50 +0900

    어드민 모듈 생성

---
+++ admin/src/main/java/springproject/BadmintonAdminApplication.java
@@ -0,0 +1,11 @@
+package springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadmintonAdminApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BadmintonAdminApplication.class, args);
+	}
+}
