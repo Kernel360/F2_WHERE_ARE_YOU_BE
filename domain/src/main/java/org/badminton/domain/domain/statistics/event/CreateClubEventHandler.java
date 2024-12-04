8330eabba41f6a9555124d413ddd0a92792aa89c
soeunnPark <carol0724@naver.com>
2024-12-04 10:08:47 +0900

    refactor: @Async 어노테이션 추가

--- domain/src/main/java/org/badminton/domain/domain/statistics/event/CreateClubEventHandler.java
+++ domain/src/main/java/org/badminton/domain/domain/statistics/event/CreateClubEventHandler.java
@@ -1,6 +1,7 @@
 package org.badminton.domain.domain.statistics.event;
 
 import org.badminton.domain.domain.statistics.ClubStatisticsService;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -11,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 public class CreateClubEventHandler {
 	private final ClubStatisticsService clubStatisticsService;
 
+	@Async
 	@TransactionalEventListener
 	public void createClubEventListener(CreateClubEvent event) {
 		clubStatisticsService.createStatistic(event.getClubCreateInfo());
